06f77bee62b28fef721d39ef484f3e8b6f384192
zaksang <zaksang@gmail.com>
2017-11-11 11:19:48 -0800

    seconds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Jamaican Jerk Burger');
+INSERT INTO burgers (burger_name) VALUES ('Louisiana Gombo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Le Burger de Paris');
