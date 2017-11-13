62b7c48a48218fda7578be6e309c0e8afbe630bb
vanmax75 <zaksang@gmail.com>
2017-11-13 15:23:24 -0800

    heroku fix

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-var Sequelize = require('sequelize'), connection;
-if (process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  connection = new Sequelize('burgers_db', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3000',
-    password: "admin"
-  })
-}
+// var Sequelize = require('sequelize'), connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   connection = new Sequelize('burgers_db', 'root', 'password', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: '3000'
+//   })
+// }
 
 var express = require('express');
 var bodyParser = require('body-parser');
