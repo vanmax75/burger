0cecc36f9aaa2467a2660a147ad5d01cd411628e
vanmax75 <zaksang@gmail.com>
2017-11-13 15:21:28 -0800

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-// var Sequelize = require('sequelize'), connection;
-// if (process.env.JAWSDB_URL) {
-//   connection = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   connection = new Sequelize('burgers_db', 'root', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: '3000'
-//   })
-// }
+var Sequelize = require('sequelize'), connection;
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize('burgers_db', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3000',
+    password: "admin"
+  })
+}
 
 var express = require('express');
 var bodyParser = require('body-parser');
